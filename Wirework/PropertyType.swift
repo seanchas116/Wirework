219a425f2df17e8ea8f5137a76031c9612cb4738
Ryohei Ikegami <iofg2100@gmail.com>
2016-02-02 18:36:06 +0900

    Add property tests

--- Wirework/PropertyType.swift
+++ Wirework/PropertyType.swift
@@ -12,10 +12,14 @@ extension PropertyType {
     }
 }
 
-public func combine<T1: PropertyType, T2: PropertyType>(p1: T1, p2: T2) -> Property<(T1.Value, T2.Value)> {
+public func combine<T1: PropertyType, T2: PropertyType>(p1: T1, _ p2: T2) -> Property<(T1.Value, T2.Value)> {
     let changed = merge(
         p1.changed.map { _ in (p1.value, p2.value) },
         p2.changed.map { _ in (p1.value, p2.value) }
     )
     return AdapterProperty(changed) { (p1.value, p2.value) }
+}
+
+public func combine<T1: PropertyType, T2: PropertyType, U>(p1: T1, _ p2: T2, transform: (T1.Value, T2.Value) -> U) -> Property<U> {
+    return combine(p1, p2).map(transform)
 }
--- WireworkTests/PropertySpec.swift
+++ WireworkTests/PropertySpec.swift
@@ -14,12 +14,24 @@ import Wirework
 class PropertySpec: QuickSpec {
     override func spec() {
         describe("Property") {
-            it("binds value") {
-                let foo = Variable("foo")
-                let length = foo.map { $0.characters.count }
-                expect(length.value).to(equal(3))
-                foo.value = "hogehoge"
-                expect(length.value).to(equal(8))
+            describe("map") {
+                it("transforms value") {
+                    let foo = Variable("foo")
+                    let length = foo.map { $0.characters.count }
+                    expect(length.value).to(equal(3))
+                    foo.value = "hogehoge"
+                    expect(length.value).to(equal(8))
+                }
+            }
+            describe("combine") {
+                it("combines values") {
+                    let firstName = Variable("Foo")
+                    let lastName = Variable("Bar")
+                    let fullName = combine(firstName, lastName) { "\($0) \($1)" }
+                    expect(fullName.value).to(equal("Foo Bar"))
+                    firstName.value = "Piyo"
+                    expect(fullName.value).to(equal("Piyo Bar"))
+                }
             }
         }
     }
