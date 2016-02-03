eaa30f291f7734cf343acb92cd8169da7e1ff9f8
Ryohei Ikegami <iofg2100@gmail.com>
2016-02-04 01:20:33 +0900

    Add WireworkFoundation targets

---
+++ WireworkFoundationTests/WireworkFoundationSpec.swift
@@ -0,0 +1,17 @@
+//
+//  WireworkFoundationTests.swift
+//  WireworkFoundationTests
+//
+//  Created by Ryohei Ikegami on 2016/02/04.
+//
+//
+
+import Quick
+import Nimble
+import WireworkFoundation
+
+class WireworkFoundationSpec: QuickSpec {
+    override func spec() {
+        
+    }
+}
