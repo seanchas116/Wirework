6fd6493ab846a460c25175692dd176fa4a47e81b
Ryohei Ikegami <iofg2100@gmail.com>
2016-02-05 20:15:28 +0900

    Fix comment

--- Wirework/Wirework.h
+++ Wirework/Wirework.h
@@ -14,6 +14,6 @@ FOUNDATION_EXPORT double WireworkVersionNumber;
 //! Project version string for Wirework.
 FOUNDATION_EXPORT const unsigned char WireworkVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Wirework_iOS/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <Wirework/PublicHeader.h>
 
 
