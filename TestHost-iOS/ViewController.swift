c2969352d9d1c22dc91acace7602580527088be4
Ryohei Ikegami <iofg2100@gmail.com>
2016-02-06 18:20:09 +0900

    Add test host for iOS

---
+++ TestHost-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestHost-iOS
+//
+//  Created by Ryohei Ikegami on 2016/02/06.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
