17f8ee8e30c2f8937574d53d07c15b04afaf48f8
Ryohei Ikegami <iofg2100@gmail.com>
2016-03-03 15:22:25 +0900

    Add UIBarButtonItem#wwTapped

---
+++ WireworkUIKitTests/UIBarButtonItemSpec.swift
@@ -0,0 +1,24 @@
+import UIKit
+import Quick
+import Nimble
+import Wirework
+import WireworkUIKit
+
+class UIBarButtonItemSpec: QuickSpec {
+    override func spec() {
+        describe("UIBarButtonItem") {
+            describe("wwTapped") {
+                it("emits when tapped", andCleansUpResources: true) {
+                    let bag = SubscriptionBag()
+                    let item = UIBarButtonItem()
+                    var tapped = false
+                    item.wwTapped.subscribe {
+                        tapped = true
+                    }.addTo(bag)
+                    item.target?.performSelector(item.action)
+                    expect(tapped).to(beTrue())
+                }
+            }
+        }
+    }
+}
